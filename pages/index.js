117f2417de72ee70a4c6b0bad12ef1d8e17a111f
abishek poudel chhetri <neesedabhi@gmail.com>
2021-11-17 21:22:27 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to ChimekiBhai!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get Help From Pros
         </p>
       </main>
 
